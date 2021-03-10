0bf0694f543ec463056b4e980dcef34310eb3547
NicholasNeto <nicholasneto@outlook.com>
2021-03-10 18:00:54 -0300

    used useEffect in componet repositoryList

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,6 @@
+import {useState, useEffect } from 'react'
 import { RepositoryItem } from './RepositoryItem';
+import '../sytles/repositories.scss';
 
 const repository = {
     name: 'unform2',
@@ -7,6 +9,15 @@ const repository = {
 }
 
 export function RepositoryList() {
+
+    const [ repositories, setRepositories] = useState()
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(reposnse => reposnse.json())
+            .then(data => setRepositories(data))
+    }, [])
+
     return (
         <section className='repository-list'>
             <h1>Lista de repositorios</h1>
