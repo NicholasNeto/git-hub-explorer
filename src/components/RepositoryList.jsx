9969d6cce5dfab95dacd1629e2ce222693cc7a7d
NicholasNeto <nicholasneto@outlook.com>
2021-03-10 07:24:26 -0300

    fisrt component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+export function RepositoryList() {
+    return (
+        <section className='repository-list'>
+            <h1>Lista de repositorios</h1>
+
+            <ul>
+                <li>
+                    <strong>unformato</strong>
+                    <p>Forms in React</p>
+                    <a href=""> Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>unformato</strong>
+                    <p>Forms in React</p>
+                    <a href=""> Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>unformato</strong>
+                    <p>Forms in React</p>
+                    <a href=""> Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>unformato</strong>
+                    <p>Forms in React</p>
+                    <a href=""> Acessar repositorio</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
