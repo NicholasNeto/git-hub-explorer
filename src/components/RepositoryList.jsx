1b4eff636cd0160fc11728ff155cb880f7620a85
NicholasNeto <nicholasneto@outlook.com>
2021-03-10 18:11:38 -0300

    get all repository

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,15 +2,9 @@ import {useState, useEffect } from 'react'
 import { RepositoryItem } from './RepositoryItem';
 import '../sytles/repositories.scss';
 
-const repository = {
-    name: 'unform2',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
 
-    const [ repositories, setRepositories] = useState()
+    const [ repositories, setRepositories] = useState([])
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -18,14 +12,12 @@ export function RepositoryList() {
             .then(data => setRepositories(data))
     }, [])
 
+
     return (
         <section className='repository-list'>
             <h1>Lista de repositorios</h1>
             <ul>
-              <RepositoryItem repository={repository}/>
-              <RepositoryItem repository={repository} />
-              <RepositoryItem repository={repository} />
-              <RepositoryItem repository={repository} />
+                {repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />)}
             </ul>
         </section>
     );
